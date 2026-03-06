90addbaa60e54ef20b861eb38e389b54214239fe
gaurav0031 <gauravpratap2631@gmail.com>
2026-03-06 14:11:24 +0530

    arrays

---
+++ Arrays/reomveduplicate.js
@@ -0,0 +1,5 @@
+//Write a function that removes the duplicates from a sorted array in non-decreasing order.We don't a new array to be created, we need to do this in place with O(1) extra memory. Return the length of the array after duplicates have been removed.
+
+var removeDuplicates = function(nums) {
+
+}
